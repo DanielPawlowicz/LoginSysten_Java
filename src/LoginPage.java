aff147854ba02c1a4c2a8e9b2a444bd4687a79de
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-15 11:38:07 +0100

    Creating login page - adding labels

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -2,7 +2,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class LoginPage implements ActionListener{
+	
+	JFrame frame = new JFrame();
+	JButton loginButton = new JButton("Login");
+	JButton resetButton = new JButton("Reset");
+	JTextField userIDField = new JTextField();
+	JTextField userPasswordField = new JTextField();
+	JLabel userIDLabel = new JLabel("user ID: ");
+	JLabel userPasswordLabel = new JLabel("password: ");
+	JLabel messageLabel = new JLabel();
+	
 	
 	HashMap<String, String> logininfo = new HashMap<String, String>();
 	
@@ -10,6 +25,15 @@ public class LoginPage implements ActionListener{
 		
 		logininfo = loginInfoOriginal;
 		
+		userIDLabel.setBounds(50,100,75,25);
+		userPasswordLabel.setBounds(50,150,75,25);
+		
+		frame.add(userIDLabel);
+		frame.add(userPasswordLabel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		frame.setVisible(true);
 	}
 
 	@Override
